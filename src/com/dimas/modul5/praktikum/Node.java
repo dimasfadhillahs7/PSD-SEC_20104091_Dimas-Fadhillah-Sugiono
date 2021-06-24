7e08f7399d97796a6f0727bf23c1de05c2393dc7
dimasfadhillahs7 <dimas.fadhillahs7@gmail.com>
2021-06-24 10:23:22 +0700

    node and Db_LinkedList

---
+++ src/com/dimas/modul5/praktikum/Node.java
@@ -0,0 +1,10 @@
+package com.dimas.modul5.praktikum;
+    public class Node {
+        public Node kanan, kiri;
+        public Object data;
+        public Node(Object a){
+        this.data = a;
+        }
+    }
+
+
