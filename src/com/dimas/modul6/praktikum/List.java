22054610c34824f42aeb60a2f560ce0bff03dbc0
dimasfadhillahs7 <dimas.fadhillahs7@gmail.com>
2021-06-25 21:49:40 +0700

    Stack code for practice

---
+++ src/com/dimas/modul6/praktikum/List.java
@@ -0,0 +1,11 @@
+package com.dimas.modul6.praktikum;
+public interface List {
+    boolean isEmpty();
+    void push(Node x);
+    void pop();
+    void show();
+    void top();
+    void topAndPop();
+    void makeEmpty();
+    
+}
---
+++ src/com/dimas/modul6/praktikum/Node.java
@@ -0,0 +1,11 @@
+package com.dimas.modul6.praktikum;
+public class Node {
+    Object data;
+    Node next;
+    
+    public Node(Object a) {
+        this.data = a;
+    }
+  
+}
+
