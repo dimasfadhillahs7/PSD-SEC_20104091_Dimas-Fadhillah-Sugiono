d212c927eef8e0ea219719d6bf304a77c985d4c0
dimasfadhillahs7 <dimas.fadhillahs7@gmail.com>
2021-07-27 21:08:22 +0700

    Modul 10

---
+++ src/com/dimas/modul10/praktikum/sorting.java
@@ -0,0 +1,48 @@
+package com.dimas.modul10.praktikum;
+public class sorting {
+    static int temp, pos, i, j;
+
+    public static int[] bubbleSort (int data[]) {
+        for (i = 1; i < data.length; i++) {
+            for (j = data.length-1; j >= i; j--){
+                if (data[j] < data[j-1]){
+                    temp = data[j];
+                    data[j] = data[j - 1];
+                    data[j - 1] = temp;
+                }
+            }
+        }
+        return data;
+    }
+
+    public static int [] selectionSort(int data[]){
+        for (i = 0; i < data.length - 1; i++){
+            pos = i;
+            for (j = i + 1; j < data.length; j++){
+                if (data[j] < data[pos]){
+                    pos = j;
+                }
+            }
+            if (pos != i){
+                temp = data[pos];
+                data[pos] = data[i];
+                data[i] = temp;
+            }
+        }
+        return data;
+    }
+
+    public static int[] insertSort(int data[]){
+        for (i = 1; i < data.length; i++){
+            temp = data[i];
+            j = i;
+            while ((j > 0) && (data[j-1] > temp)) {
+                data[j] = data[j - 1];
+                j--;
+            }
+            data[j] = temp;
+        }
+        return data;
+    }
+}
+    
